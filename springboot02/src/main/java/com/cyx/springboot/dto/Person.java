9e4fa1d78833382555aa4f06935143cd4d2dceea
jy02426305 <88042711@qq.com>
2019-04-07 23:10:51 +0800

    yaml自定义对象填充；

---
+++ springboot02/src/main/java/com/cyx/springboot/dto/Person.java
@@ -0,0 +1,69 @@
+package com.cyx.springboot.dto;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+@Component
+@ConfigurationProperties(prefix = "person")
+public class Person {
+    private String name;
+    private int age;
+    private Map<String,String> maps;
+    private List<String> list;
+
+    private Dog dog;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Map<String, String> getMaps() {
+        return maps;
+    }
+
+    public void setMaps(Map<String, String> maps) {
+        this.maps = maps;
+    }
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+
+    public Dog getDog() {
+        return dog;
+    }
+
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", maps=" + maps +
+                ", list=" + list +
+                ", dog=" + dog +
+                '}';
+    }
+}
--- springboot02/src/test/java/com/cyx/springboot/Springboot02ApplicationTests.java
+++ springboot02/src/test/java/com/cyx/springboot/Springboot02ApplicationTests.java
@@ -1,16 +1,19 @@
 package com.cyx.springboot;
 
+import com.cyx.springboot.dto.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Springboot02ApplicationTests {
-
+    @Autowired
+    Person person;
     @Test
     public void contextLoads() {
+        System.out.println(person.toString());
     }
-
 }
