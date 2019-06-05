1327b05b2c43311e13a417e7fa1ca3092b64b8a4
caiyuanxing <88042711@qq.com>
2019-06-05 23:24:31 +0800

    rabbitmq

---
+++ springboot11-rabbitmq-sender/src/main/java/cn/cyx/springboot/Springboot11RabbitmqSenderApplication.java
@@ -0,0 +1,13 @@
+package cn.cyx.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot11RabbitmqSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot11RabbitmqSenderApplication.class, args);
+    }
+
+}
