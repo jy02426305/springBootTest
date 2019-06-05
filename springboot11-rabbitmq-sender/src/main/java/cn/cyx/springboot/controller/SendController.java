bad49c68d3989cad500a21ce2f5e36a9e6471c95
caiyuanxing <88042711@qq.com>
2019-06-05 23:23:23 +0800

    rabbitmq

---
+++ springboot11-rabbitmq-sender/src/main/java/cn/cyx/springboot/controller/SendController.java
@@ -0,0 +1,21 @@
+package cn.cyx.springboot.controller;
+
+import cn.cyx.springboot.service.FirstSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+public class SendController {
+    @Autowired
+    private FirstSender firstSender;
+
+    @GetMapping("/send")
+    public String send(String message){
+        String uuid = UUID.randomUUID().toString();
+        firstSender.send(uuid,message);
+        return uuid;
+    }
+}
---
+++ springboot11-rabbitmq-sender/src/main/java/cn/cyx/springboot/service/FirstSender.java
@@ -0,0 +1,24 @@
+package cn.cyx.springboot.service;
+
+import cn.cyx.springboot.config.RabbitMqConfig;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FirstSender {
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    /**
+     * 发送消息
+     * @param uuid
+     * @param message  消息
+     */
+    public void send(String uuid,Object message) {
+        CorrelationData correlationId = new CorrelationData(uuid);
+        rabbitTemplate.convertAndSend(RabbitMqConfig.EXCHANGE, RabbitMqConfig.ROUTINGKEY2,
+                message, correlationId);
+    }
+}
