a939b63f64cac4bf260e824e95ff7777e555749e
jy02426305 <88042711@qq.com>
2019-07-17 17:54:34 +0800

    cas

---
+++ springboot13-cas-client2/src/main/java/com/cyx/springboot/CasClient2Application.java
@@ -0,0 +1,15 @@
+package com.cyx.springboot;
+
+import net.unicon.cas.client.configuration.EnableCasClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableCasClient
+public class CasClient2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasClient2Application.class, args);
+    }
+
+}
