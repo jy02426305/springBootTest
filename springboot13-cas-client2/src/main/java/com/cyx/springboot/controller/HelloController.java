6faac961e7b32f311cbbc3b7b77abb3de600966b
jy02426305 <88042711@qq.com>
2019-07-19 18:01:13 +0800

    忘了

--- springboot13-cas-client2/src/main/java/com/cyx/springboot/controller/HelloController.java
+++ springboot13-cas-client2/src/main/java/com/cyx/springboot/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @GetMapping(value = "/hello2")
-    public String hello2(){
+    public String hello2() {
         return "hello2";
     }
 }
